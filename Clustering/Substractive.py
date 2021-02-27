c2847fe297c82c3863013f74d2bd27fbfae74b41
Pablo Osorio Lopez <osoriopabl@gmail.com>
2021-02-27 14:45:10 -0500

    Correct substract.

--- Clustering/Substractive.py
+++ Clustering/Substractive.py
@@ -11,24 +11,24 @@ def substraction_function( X, v, denominator):
     return np.sum( [ density_function( x, v, denominator) for x in X ] )
 
 def calculate_substraction_vector(X, ra):
-    return [ substraction_function( X, x, sigma) for x in X ]
+    return [ substraction_function( X, x, denominator=(ra/2)**2 ) for x in X ]
 
 def select_first_center(M, V):
     return V[ np.argmax(M) ]
 
-def update_substraction( X, V, M, c, rb):
+def update_substraction( X, M, c, rb):
     M_c = np.max(M)
     for i,_ in enumerate(M):
-        M[i] -= M_c * density_function( V[i], c, beta )
+        M[i] -= M_c * density_function( X[i], c, denominator=(rb/2)**2  )
     return M
 
 def substract(X, num_clusters=2, num_divisions=1, ra=1.0, rb=None):
     if not rb:
         rb = 1.5 * ra
-    M = calculate_substraction_vector( X, denominator=(ra/2)**2 )
+    M = calculate_substraction_vector(X, ra)
     C = []
     for i in range(num_clusters):
         c = select_first_center(M, X)
-        M = update_substraction(X, M, c, denominator=(ra/2)**2)
+        M = update_substraction(X, M, c, rb)
         C.append( c )
     return C
