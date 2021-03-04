349ae4445df68625c7a427089e338adc9e54a8a5
Pablo Osorio Lopez <osoriopabl@gmail.com>
2021-03-04 09:22:55 -0500

    Refactor agglomerative to return labels.

--- Clustering/Agglomerative.py
+++ Clustering/Agglomerative.py
@@ -1,7 +1,9 @@
 import numpy as np
 
+
 def distance(x, y):
     return np.linalg.norm(np.subtract(x, y))
+
 
 def calc_distance_clusters(clusters):
     n = len(clusters)
@@ -13,23 +15,28 @@ def calc_distance_clusters(clusters):
             distances[j, i] = dij
     return distances
 
+
 class Cluster:
-    def __init__(self, X):
-        assert X.ndim == 2 #  X must have to be matrix
+    def __init__(self, X, indexes):
+        assert X.ndim == 2  #  X must have to be matrix
         self.points = X
         self.center = np.mean(X, axis=0)
-    
+        self.indexes = indexes
+
     def distance(self, other_cluster):
         return distance(self.center, other_cluster.center)
 
     def join(self, other_cluster):
         new_X = np.vstack([self.points, other_cluster.points])
-        return Cluster(new_X)         
+        new_indexes = [*self.indexes, *other_cluster.indexes]
+        return Cluster(new_X, new_indexes)
+
 
 def select_clusters_to_join(distance_matrix):
     masked_distance = distance_matrix.copy()
     np.fill_diagonal(masked_distance, np.inf)
     return np.unravel_index(masked_distance.argmin(), masked_distance.shape)
+
 
 def join_clusters(clusters, i, j):
     new_cluster = clusters[i].join(clusters[j])
@@ -42,6 +49,7 @@ def join_clusters(clusters, i, j):
     clusters.append(new_cluster)
     return clusters
 
+
 def update_clusters_distances(i, j, distance_clusters, clusters):
     assert i != j
     # Delete rows-cols representing distances to clusters i and j
@@ -53,7 +61,9 @@ def update_clusters_distances(i, j, distance_clusters, clusters):
     # Add new row that represents distance from new cluster (last)
     # to the others
     new_cluster = clusters[-1]
-    last_cluster_dists = [new_cluster.distance(o_cluster) for o_cluster in clusters[: -1]]
+    last_cluster_dists = [
+        new_cluster.distance(o_cluster) for o_cluster in clusters[:-1]
+    ]
     # Add new col
     col_to_concat = np.array([last_cluster_dists]).T
     new_distances = np.hstack([new_distances, col_to_concat])
@@ -61,9 +71,10 @@ def update_clusters_distances(i, j, distance_clusters, clusters):
     new_distances = np.vstack([new_distances, row_to_concat])
     return new_distances
 
+
 def agglomerative(X, num_clusters=2):
     n = len(X)
-    clusters = [ Cluster(X[[i]]) for i in range(n)]
+    clusters = [Cluster(X[[i]], [i]) for i in range(n)]
     distance_clusters = calc_distance_clusters(clusters)
     linkages = []
     while len(clusters) > num_clusters:
@@ -71,4 +82,9 @@ def agglomerative(X, num_clusters=2):
         linkages.append([i, j])
         clusters = join_clusters(clusters, i, j)
         distance_clusters = update_clusters_distances(i, j, distance_clusters, clusters)
-    return np.array( [cluster.points for cluster in clusters] )
+    # Get labels of data original order
+    labels = np.zeros((n, 1), dtype=int)
+    for num_cluster, cluster in enumerate(clusters):
+        indexes_clt = cluster.indexes
+        labels[indexes_clt] = num_cluster
+    return labels.T
