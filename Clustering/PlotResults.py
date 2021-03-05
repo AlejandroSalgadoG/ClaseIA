4d67cfc6f246946c4b246c43f60ed357633e3f66
Pablo Osorio Lopez <osoriopabl@gmail.com>
2021-03-05 17:41:23 -0500

    Delete black edge in plot3D.

--- Clustering/PlotResults.py
+++ Clustering/PlotResults.py
@@ -45,12 +45,12 @@ def plot_result3d( X, M, C=None, U=None, features=[0,1,2] ):
     X1, X2, X3 = extract_features( X, features )
     if U is None: x_colors = [ get_plt_color(m) for m in M ]
     else: x_colors = [ get_plt_color(m, alpha=u) for m,u in zip( M, U ) ]
-    ax.scatter(X1, X2, X3, c=x_colors, edgecolors='k', depthshade=False)
+    ax.scatter(X1, X2, X3, c=x_colors, depthshade=False)
 
     if C is not None:
         C1, C2, C3 = extract_features( C, features )
         c_colors = [ get_plt_color(i) for i,_ in enumerate(C) ]
-        ax.scatter(C1, C2, C3, c=c_colors, edgecolors='k', marker="X", s=100, depthshade=False )
+        ax.scatter(C1, C2, C3, c=c_colors, marker="X", s=100, depthshade=False )
 
     ax.set_xlabel("f1")
     ax.set_ylabel("f2")
