7a3ada7c1cdd2a744b7bf6ecf39c463c274600f0
Alejandro Salgado G <asalgad2@eafit.edu.co>
2021-04-18 09:27:50 -0500

    Adding performance indicators to NNet train

--- Supervised/TrainNet.py
+++ Supervised/TrainNet.py
@@ -1,10 +1,11 @@
 import numpy as np
+np.random.seed(1234567)
 
 from NNet.NNet import NNet
 
-from sklearn.metrics import confusion_matrix
 from sklearn.datasets import load_breast_cancer
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix, roc_auc_score
 
 def normalize( x ):
     return (x - x.mean()) / x.std()
@@ -30,6 +31,15 @@ def hard_classification( y_hat, threshold=0.5 ):
     y_hard[ y_hat > threshold ] = 1
     return y_hard
 
+def accuracy( tn, fp, fn, tp ):
+    return (tn+tp) / (tn+fp+fn+tp)
+
+def sensitivity( fn, tp ):
+    return tp / (tp+fn)
+
+def specificity( tn, fp ):
+    return tn / (tn+fp)
+
 dataset = load_breast_cancer( as_frame=True )
 
 x,y = dataset["data"], dataset["target"]
@@ -39,7 +49,7 @@ x_train, x_test, y_train, y_test = train_test_split( x, y, test_size=0.3, shuffl
 epoch = 500
 tol = 1e-2
 
-arch = [30, 15, 2]
+arch = [30, 20, 15, 2]
 eta = 0.2
 batch_size = 100
 
@@ -60,5 +70,18 @@ print()
 y_hat = np.array( [ nnet.predict(input_data, weights) for input_data in x_test ] )
 y_hard = hard_classification( y_hat, threshold=0.5 )
 
-conf_matrix = confusion_matrix( binary2class( y_test ), binary2class( y_hard ) )
-print( conf_matrix )
+y_true = binary2class( y_test )
+y_pred = binary2class( y_hard )
+
+conf_matrix = confusion_matrix( y_true, y_pred )
+tn, fp, fn, tp = conf_matrix.ravel()
+
+accu = accuracy( tn, fp, fn, tp )
+sens = sensitivity( fn, tp )
+spec = specificity( tn, fp )
+roc_auc = roc_auc_score( y_true, y_pred )
+
+print( "accuracy", "%.6f" % accu )
+print( "sensitivity", "%.6f" % sens )
+print( "specificity", "%.6f" % spec )
+print( "roc_auc", "%.6f" % roc_auc )
